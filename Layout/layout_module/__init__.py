bc40627599e215fca7759da763bc71f7d95108ba
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-01-15 12:25:35 +0530

    asd

--- Layout/layout_module/__init__.py
+++ Layout/layout_module/__init__.py
@@ -3,3 +3,4 @@ from .nested_tuple import NestedTuple
 from . import flat_algebra, nested_tuple_algebra, layout_algebra
 from .layout import Layout
 from .flat_algebra import Pointed
+from .composition import Flat_tuple_morphism
