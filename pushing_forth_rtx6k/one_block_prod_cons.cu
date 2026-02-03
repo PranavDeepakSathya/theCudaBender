213a1c7eec42ea813b75564ac3595eaf652f3b13
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-02-03 22:22:43 +0000

    asfd

--- pushing_forth_rtx6k/one_block_prod_cons.cu
+++ pushing_forth_rtx6k/one_block_prod_cons.cu
@@ -2,21 +2,18 @@
 constexpr int mma_m = 16; 
 constexpr int mma_n = 8;
 constexpr int mma_k = 16; 
-constexpr int acc_per_warp_m = 2; 
-constexpr int acc_per_warp_n = 2; 
+constexpr int acc_per_warp_m = 4; 
+constexpr int acc_per_warp_n = 4; 
 constexpr int num_mma_k_iters = 2;
 constexpr int WM = mma_m*acc_per_warp_m; 
 constexpr int WN = mma_n*acc_per_warp_n;
 constexpr int BK = mma_k*num_mma_k_iters;
-constexpr int warps_per_block_m = 2; 
-constexpr int warps_per_block_n = 2; 
+constexpr int warps_per_block_m = 4; 
+constexpr int warps_per_block_n = 4; 
 constexpr int BM = warps_per_block_m*WM; 
-constexpr int BN = warps_per_block_n*WN;
-constexpr int BM_iters = 2; 
-constexpr int BN_iters = 2; 
-constexpr int total_block_iters = BM_iters*BN_iters; 
-constexpr int M = BM_iters*BM; 
-constexpr int N = BN_iters*BN; 
+constexpr int BN = warps_per_block_n*WN; 
+constexpr int M = BM; 
+constexpr int N = BN; 
 constexpr int K = 4096; 
 
 
@@ -49,29 +46,24 @@ __global__ void matmul(__grid_constant__ const CUtensorMap gA,
   float* C)
 
 {
-
-
   nv_bfloat16* As[bk_stages]; 
   nv_bfloat16* Bs[bk_stages]; 
+
   uint32_t smem_base_a[bk_stages];
   uint32_t smem_base_b[bk_stages];
 
 
   extern __shared__ uint8_t smem_raw[];
-  allocate_smem_tiles(smem_raw, As_bytes, Bs_bytes,
+  allocate_smem_tiles(smem_raw, As_bytes, Bs_bytes, 
     bk_stages, As, Bs,  smem_base_a, smem_base_b);
      
   __shared__ barrier full[bk_stages], empty[bk_stages];
-
-
-
 
   int t = threadIdx.x; 
   int w = t/32; 
   int l = t%32; 
   int warp_start_m = (w / warps_per_block_n)*WM; 
   int warp_start_n = (w % warps_per_block_n)*WN; 
-  
 
   if (threadIdx.x == 0) {
   for (int i = 0; i < bk_stages; ++i) {
@@ -79,156 +71,144 @@ __global__ void matmul(__grid_constant__ const CUtensorMap gA,
       init(&empty[i],(n_consumer_warps*32) + 1);
   }
   ptx::fence_proxy_async(ptx::space_shared);
-  }
+}
 
   __syncthreads();
-  
-  for (int blk_iter = 0; blk_iter < total_block_iters; blk_iter++)
+
+  //producer 
+  if (w == prod_warp_id)
   {
-
-
-    int blk_iter_m = (blk_iter/BN_iters)*BM; 
-    int blk_iter_n = (blk_iter%BN_iters)*BN; 
-
-    if (w == prod_warp_id)
+    if (t == p_thread_id)
     {
-      if (t == p_thread_id)
-      {
-        int stage = 0; 
-        #pragma unroll
-        for (int bk = 0; bk < num_BK_iters; ++bk, ++stage)
-        {
-          if (stage ==bk_stages) stage = 0;
-          empty[stage].wait(empty[stage].arrive());
-
-          int32_t coordsA[2] = {bk*BK, blk_iter_m};
-          int32_t coordsB[2] = {bk*BK, blk_iter_n};
-
-            ptx::cp_async_bulk_tensor(
-                ptx::space_shared, ptx::space_global,
-                As[stage], &gA, coordsA,
-                cuda::device::barrier_native_handle(full[stage]));
-
-            ptx::cp_async_bulk_tensor(
-                ptx::space_shared, ptx::space_global,
-                Bs[stage], &gB, coordsB,
-                cuda::device::barrier_native_handle(full[stage]));
-
-            barrier::arrival_token _ = cuda::device::barrier_arrive_tx(
-                full[stage], 1, As_bytes + Bs_bytes);
-
-        }
-      }
-    }
-    else//consumer logic 
-    {
-      #pragma unroll
-      for (int i = 0; i < bk_stages; ++i) {
-            // i initially, all buffers are considered empty; ready for write
-            barrier::arrival_token _ = empty[i].arrive();
-        }
-      
       int stage = 0; 
-
-      int a_lane_row_base = l % 16;
-      int a_lane_col_base = (l / 16) * 8;
-
-      int b_lane_row_base = 8*(l/8);
-      int b_lane_col_base = l % 8; 
-
-      uint32_t ra[acc_per_warp_m*4]; //the access is (wm,reg_id) -> 4*wm + reg_id 
-      uint32_t rb[acc_per_warp_n*2]; //the access is (wn, reg_id) -> 2*wn + reg_id 
-      float rc[acc_per_warp_m*acc_per_warp_n*4] = {0.0f}; //the access is (wm,wn,reg_id) -> acc_per_warp_n*wm*4 + wn*4 + reg_id 
       #pragma unroll
       for (int bk = 0; bk < num_BK_iters; ++bk, ++stage)
       {
-        if (stage == bk_stages) stage = 0;
-        full[stage].wait(full[stage].arrive());
-        
+        if (stage ==bk_stages) stage = 0;
+        empty[stage].wait(empty[stage].arrive());
+
+        int32_t coordsA[2] = {bk*BK, 0};
+        int32_t coordsB[2] = {bk*BK, 0};
+
+          ptx::cp_async_bulk_tensor(
+              ptx::space_shared, ptx::space_global,
+              As[stage], &gA, coordsA,
+              cuda::device::barrier_native_handle(full[stage]));
+
+          ptx::cp_async_bulk_tensor(
+              ptx::space_shared, ptx::space_global,
+              Bs[stage], &gB, coordsB,
+              cuda::device::barrier_native_handle(full[stage]));
+
+          barrier::arrival_token _ = cuda::device::barrier_arrive_tx(
+              full[stage], 1, As_bytes + Bs_bytes);
+
+      }
+    }
+  }
+  else//consumer logic 
+  {
+    #pragma unroll
+    for (int i = 0; i < bk_stages; ++i) {
+          // i initially, all buffers are considered empty; ready for write
+          barrier::arrival_token _ = empty[i].arrive();
+      }
+    
+    int stage = 0; 
+
+    int a_lane_row_base = l % 16;
+    int a_lane_col_base = (l / 16) * 8;
+
+    int b_lane_row_base = 8*(l/8);
+    int b_lane_col_base = l % 8; 
+
+    uint32_t ra[acc_per_warp_m*4]; //the access is (wm,reg_id) -> 4*wm + reg_id 
+    uint32_t rb[acc_per_warp_n*2]; //the access is (wn, reg_id) -> 2*wn + reg_id 
+    float rc[acc_per_warp_m*acc_per_warp_n*4] = {0.0f}; //the access is (wm,wn,reg_id) -> acc_per_warp_n*wm*4 + wn*4 + reg_id 
+    #pragma unroll
+    for (int bk = 0; bk < num_BK_iters; ++bk, ++stage)
+    {
+      if (stage == bk_stages) stage = 0;
+      full[stage].wait(full[stage].arrive());
+      
+      #pragma unroll
+      for (int wk = 0; wk < num_mma_k_iters; wk++)
+      {
         #pragma unroll
-        for (int wk = 0; wk < num_mma_k_iters; wk++)
+        for (int wm = 0; wm < acc_per_warp_m; wm++)
         {
-          #pragma unroll
-          for (int wm = 0; wm < acc_per_warp_m; wm++)
-          {
-            int a_load_shared_row = warp_start_m + (wm*mma_m) + a_lane_row_base;
-            int a_load_shared_col = (wk*mma_k) + a_lane_col_base;
-            uint32_t a_ld_addr = (smem_base_a[stage]) + ((a_load_shared_col + (BK*a_load_shared_row))*sizeof(nv_bfloat16));
-            int a_reg_start = wm*4; 
-            warp_atom::ldmatrix_m8n8_x4_b16(ra[a_reg_start + 0], ra[a_reg_start + 1], ra[a_reg_start + 2], ra[a_reg_start + 3], a_ld_addr);
+          int a_load_shared_row = warp_start_m + (wm*mma_m) + a_lane_row_base;
+          int a_load_shared_col = (wk*mma_k) + a_lane_col_base;
+          uint32_t a_ld_addr = (smem_base_a[stage]) + ((a_load_shared_col + (BK*a_load_shared_row))*sizeof(nv_bfloat16));
+          int a_reg_start = wm*4; 
+          warp_atom::ldmatrix_m8n8_x4_b16(ra[a_reg_start + 0], ra[a_reg_start + 1], ra[a_reg_start + 2], ra[a_reg_start + 3], a_ld_addr);
 
-            
-          }
-
-          #pragma unroll
-          for (int wn = 0; wn < acc_per_warp_n; wn++)
-          {
-            int b_load_shared_row = (wk*mma_k) + b_lane_row_base; 
-            int b_load_shared_col = warp_start_n + (wn*mma_n) + b_lane_col_base;
-            uint32_t  b_ld_addr = (smem_base_b[stage]) + ((b_load_shared_row + (BK*b_load_shared_col))*sizeof(nv_bfloat16)); 
-            int b_reg_start = wn*2; 
-            warp_atom::ldmatrix_m8n8_x2_b16(rb[b_reg_start + 0], rb[b_reg_start + 1],b_ld_addr);
-            
-          }
-          
-          #pragma unroll
-          for (int wm = 0; wm < acc_per_warp_m; wm++)
-          {
-            #pragma unroll
-            for (int wn = 0; wn < acc_per_warp_n; wn++)
-            {
-              int a_reg_start = wm*4; 
-              int b_reg_start = wn*2; 
-              int c_reg_start = (wm*acc_per_warp_n*4) + (wn*4); 
-              warp_atom::mma_m16n8k16_row_col_f32_bf16(
-                rc[c_reg_start + 0], rc[c_reg_start + 1], rc[c_reg_start + 2], rc[c_reg_start + 3],
-                ra[a_reg_start + 0], ra[a_reg_start + 1], ra[a_reg_start + 2], ra[a_reg_start + 3],
-                rb[b_reg_start + 0], rb[b_reg_start + 1],
-                rc[c_reg_start + 0], rc[c_reg_start + 1], rc[c_reg_start + 2], rc[c_reg_start + 3]
-              );
-            }
-          }
           
         }
-        barrier::arrival_token _ = empty[stage].arrive();
-        
-      }
-      //storage: 
-      __syncthreads();
-      float2* C2 = reinterpret_cast<float2*>(C); 
-      int lane_row = l/4; 
-      int lane_col = l%4; 
-      int ldc2 = N/2; 
 
-      #pragma unroll
-      for (int wm = 0; wm < acc_per_warp_m; wm++)
-      {
         #pragma unroll
         for (int wn = 0; wn < acc_per_warp_n; wn++)
         {
-          int c2_global_row_v0 = blk_iter_m + warp_start_m + (wm*mma_m) + lane_row + 0;
-          int c2_global_row_v1 = c2_global_row_v0 + 8; 
-          int c_global_col_elem =
-            blk_iter_n 
-            + warp_start_n          // element space
-            + (wn * mma_n)
-            + (lane_col * 2);         // because float2
-
-          int c2_global_col = c_global_col_elem >> 1;
-          //dont worry I shall annotate both this kernels indexing and one_warp_one_mma.cu 
-          // and I shall very carefully extract the index maps and tiling isomorphisms. 
-          int c_reg_start = ((wn*4) + (wm*acc_per_warp_n*4));
-          float2 v0 = {rc[c_reg_start + 0], rc[c_reg_start + 1]}; 
-          float2 v1 = {rc[c_reg_start + 2], rc[c_reg_start + 3]}; 
-          C2[(c2_global_row_v0)*ldc2 + c2_global_col] = v0; 
-          C2[(c2_global_row_v1)*ldc2 + c2_global_col] = v1; 
+          int b_load_shared_row = (wk*mma_k) + b_lane_row_base; 
+          int b_load_shared_col = warp_start_n + (wn*mma_n) + b_lane_col_base;
+          uint32_t  b_ld_addr = (smem_base_b[stage]) + ((b_load_shared_row + (BK*b_load_shared_col))*sizeof(nv_bfloat16)); 
+          int b_reg_start = wn*2; 
+          warp_atom::ldmatrix_m8n8_x2_b16(rb[b_reg_start + 0], rb[b_reg_start + 1],b_ld_addr);
+          
         }
+        
+        #pragma unroll
+        for (int wm = 0; wm < acc_per_warp_m; wm++)
+        {
+          #pragma unroll
+          for (int wn = 0; wn < acc_per_warp_n; wn++)
+          {
+            int a_reg_start = wm*4; 
+            int b_reg_start = wn*2; 
+            int c_reg_start = (wm*acc_per_warp_n*4) + (wn*4); 
+            warp_atom::mma_m16n8k16_row_col_f32_bf16(
+              rc[c_reg_start + 0], rc[c_reg_start + 1], rc[c_reg_start + 2], rc[c_reg_start + 3],
+              ra[a_reg_start + 0], ra[a_reg_start + 1], ra[a_reg_start + 2], ra[a_reg_start + 3],
+              rb[b_reg_start + 0], rb[b_reg_start + 1],
+              rc[c_reg_start + 0], rc[c_reg_start + 1], rc[c_reg_start + 2], rc[c_reg_start + 3]
+            );
+          }
+        }
+        
       }
-    } 
-    
-  }
+      barrier::arrival_token _ = empty[stage].arrive();
+      
+    }
 
-  
+    float2* C2 = reinterpret_cast<float2*>(C); 
+    int lane_row = l/4; 
+    int lane_col = l%4; 
+    int ldc2 = N/2; 
+
+    #pragma unroll
+    for (int wm = 0; wm < acc_per_warp_m; wm++)
+    {
+      #pragma unroll
+      for (int wn = 0; wn < acc_per_warp_n; wn++)
+      {
+        int c2_global_row_v0 = warp_start_m + (wm*mma_m) + lane_row + 0;
+        int c2_global_row_v1 = c2_global_row_v0 + 8; 
+        int c_global_col_elem = 
+          + warp_start_n          // element space
+          + (wn * mma_n)
+          + (lane_col * 2);         // because float2
+
+        int c2_global_col = c_global_col_elem >> 1;
+        //dont worry I shall annotate both this kernels indexing and one_warp_one_mma.cu 
+        // and I shall very carefully extract the index maps and tiling isomorphisms. 
+        int c_reg_start = ((wn*4) + (wm*acc_per_warp_n*4));
+        float2 v0 = {rc[c_reg_start + 0], rc[c_reg_start + 1]}; 
+        float2 v1 = {rc[c_reg_start + 2], rc[c_reg_start + 3]}; 
+        C2[(c2_global_row_v0)*ldc2 + c2_global_col] = v0; 
+        C2[(c2_global_row_v1)*ldc2 + c2_global_col] = v1; 
+      }
+    }
+  } 
 
 }
 
@@ -262,7 +242,7 @@ int main()
   cudaDeviceSynchronize(); 
   C.to_host(); 
   
-  /*
+
   dim3 block(16, 16);
   dim3 grid(
       (N + block.x - 1) / block.x,
@@ -394,7 +374,6 @@ printf("\n ================ C_computed ============= \n");
        shared_allocate_bytes / 1024.0f);
 
   print_kernel_info();
-  */
 
 }
 
