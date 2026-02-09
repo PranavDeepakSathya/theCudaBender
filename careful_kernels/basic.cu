cb751a8307e81a8edc219bd78ca211046a7bb695
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-02-09 07:19:38 +0000

    simplify_is_key

--- careful_kernels/basic.cu
+++ careful_kernels/basic.cu
@@ -24,12 +24,37 @@ __global__ void matmul (__grid_constant__ const CUtensorMap gA,
     As, Bs,
     smem_base_a, smem_base_b,
     empty, full);
+  
+  
+  
 
 }
 
 
+using Cfg = GemmConfig<GemmInputs>;
 
-int main()
-{
-  return 0; 
+int main() {
+
+  // 1. Build problem (alloc + TMA)
+  auto prob = make_gemm_problem<Cfg>();
+
+  // 2. Construct launcher from config
+  NaiveLauncher launcher(
+      /*grid clusters*/  Cfg::GM * Cfg::GN,  // linear CTAs
+      /*cluster blocks*/ 1,                  // no clustering yet
+      /*threads*/        Cfg::block_threads,
+      /*smem*/           Cfg::smem_bytes
+  );
+
+  // 3. Launch kernel
+  launcher.launch(
+      matmul<Cfg>,        // kernel symbol
+      prob.tmaA,
+      prob.tmaB,
+      prob.C.d_ptr
+  );
+
+  CHECK_CUDA(cudaDeviceSynchronize());
+
+  printf("done\n");
 }
