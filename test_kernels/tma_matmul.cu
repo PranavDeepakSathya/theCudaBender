bd1339a1379756fc3a681931e8037fb06e3211fe
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2026-02-11 10:22:28 +0000

    tma_matmul_skeleton_ready

--- test_kernels/tma_matmul.cu
+++ test_kernels/tma_matmul.cu
@@ -1,6 +1,6 @@
 #include "../atoms/all.cuh"
 #include "sm_120_matmul_config.cuh"
-using default_cfg = Sm120_BF16_Gemm_Config;
+
 namespace ptx = cuda::ptx; 
 
 template <class cfg>
@@ -48,5 +48,45 @@ __global__ void matmul
     }
   }
   __syncthreads();
+
   
+
 }
+
+
+int main() 
+{
+  using Cfg = Sm120_BF16_Gemm_Config;
+
+  NaiveTensor<nv_bfloat16> A({Cfg::M, Cfg::K}, Layout::ROW_MAJOR);
+  NaiveTensor<nv_bfloat16> B({Cfg::K, Cfg::N}, Layout::COL_MAJOR);
+  NaiveTensor<float>       C({Cfg::M, Cfg::N}, Layout::ROW_MAJOR);
+
+  A.allocate(); B.allocate(); C.allocate();
+
+  A.init_pattern(MODE_RAND, DIST_FLOAT_NEG1_1);
+  B.init_pattern(MODE_RAND, DIST_FLOAT_NEG1_1);
+  C.init_pattern(MODE_ZEROS, DIST_FLOAT_NEG1_1);
+
+  A.to_device();
+  B.to_device();
+  C.to_device();
+
+  CUtensorMap a_map =
+      TmaDescriptor<nv_bfloat16>::create_2d_row_major(
+          A.d_ptr, {Cfg::M, Cfg::K}, {Cfg::BM, Cfg::BK});
+
+  CUtensorMap b_map =
+      TmaDescriptor<nv_bfloat16>::create_2d_col_major(
+          B.d_ptr, {Cfg::K, Cfg::N}, {Cfg::BK, Cfg::BN});
+
+  NaiveLauncher launcher(Cfg::grid_size, 1, Cfg::block_size, Cfg::shared_bytes);
+
+  launcher.launch(matmul<Cfg>, a_map, b_map, C.d_ptr);
+
+  cudaDeviceSynchronize();
+  C.to_host();
+
+  printf("Launch complete.\n");
+}
+
